4ceaf71469160bc0ec0484da8032a234bce6d79b
Gustavo Akira Uekita <akirauekita2002@gmail.com>
2024-08-11 15:56:51 -0300

    fix: skaffold running

--- category/src/main/java/br/com/gustavoakira/microsells/category/dto/ErrorResponseDTO.java
+++ category/src/main/java/br/com/gustavoakira/microsells/category/dto/ErrorResponseDTO.java
@@ -1,13 +1,12 @@
 package br.com.gustavoakira.microsells.category.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.Data;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public record ErrorResponseDTO (
-    @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm")
     LocalDateTime timestamp,
 
     Integer code,
